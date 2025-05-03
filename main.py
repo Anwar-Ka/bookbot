99019b6bf7b08ac3a8b62d84cd471f0809ae37ed
Anwar-Ka <anwarqssm@gmail.com>
2025-05-03 22:42:39 +0300

    Added print report template

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_no_of_words, get_no_of_characters
+from stats import get_no_of_words, get_no_of_characters, sort_characters
 
 def get_book_text(file_path):
     with open (file_path) as f:
@@ -12,5 +12,17 @@ def main(file_path):
     print(f"{no_of_words} words found in the document")
     for char in char_count:
         print(f"'{char}': {char_count[char]}")
+    
+    character_count = sort_characters(char_count)
+
+    print(f"""============ BOOKBOT ============
+    Analyzing book found at {file_path}
+    ----------- Word Count ----------
+    Found {no_of_words} total words
+    --------- Character Count -------""")
+    for char in character_count:
+        print(f"{char['char']}: {char['num']}")
+    
+    print("============= END ===============")
 
 main("./books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -6,10 +6,20 @@ def get_no_of_words(book_text):
 def get_no_of_characters(book_text):
     char_count = {}
     for char in book_text:
-        char = char.lower()
-        if char not in char_count:
-            char_count[char] = 1
-        else:
-            char_count[char] += 1
+        if char.isalpha():
+            char = char.lower()
+            if char not in char_count:
+                char_count[char] = 1
+            else:
+                char_count[char] += 1
     return char_count
             
+def sort_characters(char_count):
+    list_of_characters = []
+    for char in char_count:
+        list_of_characters.append({"char": char, "num": char_count[char]})
+    
+    def sort_on(list_of_characters):
+        return list_of_characters["num"]
+    list_of_characters.sort(key=sort_on, reverse=True)
+    return list_of_characters
